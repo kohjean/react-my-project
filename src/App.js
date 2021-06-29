a27529ea4951eb2f0840d5554edf6f78317369e7
kohjean <58461459+kohjean@users.noreply.github.com>
2021-06-29 23:30:55 +0900

    Change center display to responsive

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import { Button } from './Button';
 import { DropDownMenu } from './DropDownMenu';
 import { SearchForm } from './components/SearchForm';
 import { CatDisplay } from './components/CatDisplay';
+import media from "styled-media-query";
 
 const BackgroundText = styled.p`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -150%);
-  font-size: 4rem;
   font-weight: bold;
   color: #DCEDF8;
   text-shadow: 2px 2px 6px #BBC9D3,
   -2px -2px 6px #FDFFFF;
   pointer-events: none;
+  font-size: 4rem;
+  ${media.lessThan("small")`
+  /* screen width is less than 450px (small) */
+    font-size: 2rem;
+  `}
 `
 
 function App() {
